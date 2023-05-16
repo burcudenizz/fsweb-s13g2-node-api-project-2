9253e8e2d7812c5a256dd201c758032be8933418
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-05-16 15:22:38 +0300

    server oluşturuldu.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // server için gerekli olanları burada ayarlayın
+const express = require("express");
+const server = express();
+server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.send("Server is up and running!...");
+});
+
+module.exports = server;
 // posts router'ını buraya require edin ve bağlayın
