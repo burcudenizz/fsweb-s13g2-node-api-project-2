cbb9bea8c05781e3bd3eaa82ace068515e84867a
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-05-16 17:19:47 +0300

    delete,put işlemleri yapıldı.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -31,4 +31,64 @@ router.get("/:id", (req, res) => {
     });
 });
 
-//POST ile post ekledik.
+//POST ile yeni bir post ekledik.
+
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Lütfen gönderi için bir title ve contents sağlayın",
+    });
+  } else {
+    try {
+      let { id } = await Posts.insert({ title, contents });
+      let insersedPost = await Posts.findById(id);
+      res.status(201).json(insersedPost);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+    }
+  }
+});
+
+//PUT ile belirli bir id'ye sahip postu güncelledik.
+
+router.put("/:id", async (req, res) => {
+  try {
+    let willFoundPost = await Posts.findById(req.params.id);
+    if (!willFoundPost) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      let { title, contents } = req.body;
+      if (!title || !contents) {
+        res
+          .status(400)
+          .json({ message: "Lütfen gönderi için title ve contents sağlayın" });
+      } else {
+        let willUpdatePostId = await Posts.update(req.params.id, req.body);
+        let updatedPost = await Posts.findById(willUpdatePostId);
+        res.status(200).json(updatedPost);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Gönderi bilgileri güncellenemedi" });
+  }
+});
+
+//DELETE ile belirli bir id'ye sahip postu sildik.
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let willFoundPost = await Posts.findById(req.params.id);
+    if (!willFoundPost) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      await Posts.remove(req.params.id);
+      res.status(200).json(willFoundPost);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Gönderi silinemedi" });
+  }
+});
