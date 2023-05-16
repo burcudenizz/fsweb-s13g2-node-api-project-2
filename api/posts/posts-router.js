ab575681e42c3fece4ccd64f392054aab8db89e0
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-05-16 16:35:02 +0300

    belirli bir id için postu çektik.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -12,3 +12,23 @@ router.get("/", (req, res) => {
     })
     .catch(() => res.status(500).json({ message: "Gönderiler alınamadı" }));
 });
+
+//GET ile belirli id'deki postu çektik.
+
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((postWithId) => {
+      if (!postWithId) {
+        res
+          .status(404)
+          .json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+      } else {
+        res.json(postWithId);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Gönderi bilgisi alınamadı" });
+    });
+});
+
+//POST ile post ekledik.
